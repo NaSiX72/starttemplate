1cd8bd6df9cb6bf125280c4f15fed95162557f4c
NaSiX72 <97315437+NaSiX72@users.noreply.github.com>
2022-04-26 12:46:26 +0200

    Create ZH.php

---
+++ ZH/ZH.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset iso-8859-2>
+	<style>
+	h1 {
+		background-color:lightgreen;
+	}
+
+	p {
+		background-color:grey;
+	}
+	<style/>
+<head/>
+
+<body>
+
+<?php
+echo "Név: Csibi Nándor,Neptun kód: OW6LXN";
+$_GET<href 'Inf2ZH'>;
+
+<select k.nev, k.telszam 
+from kollega k
+group by k.nev;>
+?>
+
+</body>
+</html>
